1b01deeebf206bf3babff1f32a62740e9b75beb2
martaKopp <kopp.marta@gmail.com>
2018-08-27 15:04:17 +0200

    Book: add boolean borrow;

--- src/main/java/pl/marta/kopp/domain/Book.java
+++ src/main/java/pl/marta/kopp/domain/Book.java
@@ -13,14 +13,16 @@ public class Book {
     private Long id;
     @Column
     private String title;
+    private boolean borrow;
     @JoinColumn
     @ManyToMany(cascade = CascadeType.ALL)
     private List<Author> authors;
 
 
-    public Book(String title, List<Author> authors) {
+    public Book(String title, List<Author> authors, boolean borrow) {
         this.title = title;
         this.authors = authors;
+        this.borrow = borrow;
     }
 
     public Book() {
