8ba9a4555644311664803f6ec4b6484bff2979c1
martaKopp <kopp.marta@gmail.com>
2018-09-06 14:44:38 +0200

    changed book list

--- src/main/java/pl/marta/kopp/CreateDataBase/CreateBookStorage.java
+++ src/main/java/pl/marta/kopp/CreateDataBase/CreateBookStorage.java
@@ -14,40 +14,42 @@ public class CreateBookStorage {
 
     public static void create(BookStorage bookStorage) {
 
-        Author author = new Author("Jan", "Brzechwa");
+        Author author = new Author("Robert", "Martin");
         List<Author> authors = new ArrayList<>();
         authors.add(author);
-        Book book = new Book("Akademia Pana Kleksa", authors);
+        Book book = new Book("Clean Code", authors);
         bookStorage.add(book);
 
-        Author author1 = new Author("Henryk", "Sienkiewicz");
+        Author author1 = new Author("Kent", "Beck");
         List<Author> authors1 = new ArrayList<>();
         authors1.add(author1);
-        Book book1 = new Book("W Pustyni i w Puszczy", authors1);
+        Book book1 = new Book("Test Driven Development: By Example", authors1);
         bookStorage.add(book1);
 
-        Author author2 = new Author("Adam", "Mickiewicz");
+        Author author2 = new Author("Eric", "Freeman");
+        Author author22 = new Author(" Elisabeth", "Freeman");
         List<Author> authors2 = new ArrayList<>();
         authors2.add(author2);
-        Book book2 = new Book("Dziady", authors2);
+        authors2.add(author22);
+        Book book2 = new Book("Head First Design Patterns", authors2);
         bookStorage.add(book2);
 
-        Author author3 = new Author("Julian", "Tuwim");
+        Author author3 = new Author("Kathy", "Sierra");
         List<Author> authors3 = new ArrayList<>();
         authors3.add(author3);
-        Book book3 = new Book("Wiersze", authors3);
+        Book book3 = new Book("Head First Java", authors3);
         bookStorage.add(book3);
 
-        Author author4 = new Author("Bolesław", "Prus");
+        Author author4 = new Author("Craig", "Walls");
         List<Author> authors4 = new ArrayList<>();
         authors4.add(author4);
-        Book book4 = new Book("Katarynka", authors4);
+        Book book4 = new Book("Spring in Action", authors4);
         bookStorage.add(book4);
 
-        Author author5 = new Author("Aleksander", "Fredro");
+        Author author5 = new Author("Raoul Gabriel", "Urma");
         List<Author> authors5 = new ArrayList<>();
         authors5.add(author5);
-        Book book5 = new Book("Zemsta", authors5);
+        Book book5 = new Book("Java 8 in Action", authors5);
         bookStorage.add(book5);
 
     }}
